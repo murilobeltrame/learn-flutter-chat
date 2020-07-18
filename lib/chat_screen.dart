7705e1cdd1fd66331fb211a0cf2effdd9abdc7f7
Murilo Beltrame <murilobeltrame@gmail.com>
2020-07-17 23:38:39 -0300

    added image upload

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:chat/text_composer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -7,6 +11,24 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+
+  void _sendMessage({String text, File image}) async {
+
+    Map<String, dynamic> data = {};
+
+    if (image != null) {
+      StorageUploadTask task = FirebaseStorage.instance.ref()
+          .child(DateTime.now().millisecondsSinceEpoch.toString())
+          .putFile(image);
+      StorageTaskSnapshot taskSnapshot = await task.onComplete;
+      data['imageUrl'] = await taskSnapshot.ref.getDownloadURL();
+    }
+
+    if (text != null && text.isNotEmpty) data['text'] = text;
+
+    Firestore.instance.collection('messages').add(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +36,7 @@ class _ChatScreenState extends State<ChatScreen> {
         title: Text('Chat'),
         elevation: 0,
       ),
-      body: TextComposer(),
+      body: TextComposer(_sendMessage),
     );
   }
 }
--- lib/text_composer.dart
+++ lib/text_composer.dart
@@ -1,13 +1,31 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class TextComposer extends StatefulWidget {
+
+  TextComposer(this.sendMessage);
+
+  final Function({String text, File image}) sendMessage;
+
   @override
   _TextComposerState createState() => _TextComposerState();
 }
 
 class _TextComposerState extends State<TextComposer> {
 
+  TextEditingController _controller = TextEditingController();
+
   bool _isComposing = false;
+
+  void _sendMessage(String text) {
+    widget.sendMessage(text: text);
+    _controller.clear();
+    setState(() {
+      _isComposing = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +35,11 @@ class _TextComposerState extends State<TextComposer> {
         children: <Widget>[
           IconButton(
             icon: Icon(Icons.photo_camera),
-            onPressed: (){},
+            onPressed: () async {
+              final File imageFile = await ImagePicker.pickImage(source: ImageSource.camera);
+              if (imageFile == null) return;
+              widget.sendMessage(image: imageFile);
+            },
           ),
           Expanded(
             child: TextField(
@@ -27,12 +49,13 @@ class _TextComposerState extends State<TextComposer> {
                   _isComposing = text.isNotEmpty;
                 });
               },
-              onSubmitted: (text){},
+              onSubmitted: _sendMessage,
+              controller: _controller,
             ),
           ),
           IconButton(
             icon: Icon(Icons.send),
-            onPressed: _isComposing ? () {} : null,
+            onPressed: _isComposing ? () { _sendMessage(_controller.text); } : null,
           ),
         ],
       ),
